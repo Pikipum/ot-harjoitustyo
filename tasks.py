dcae31e6894741b02cefe823dc636a559b7a3727
markus <markus.tervonen@hotmail.com>
2021-05-04 19:17:24 +0300

    poetry working

---
+++ tasks.py
@@ -0,0 +1,22 @@
+from invoke import task
+
+
+@task
+def start(ctx):
+    ctx.run("python3 src/chessgame.py")
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest")
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage.html")
+
+@task
+def test(ctx):
+    ctx.run("pytest src")
+
+@task
+def lint(ctx):
+    ctx.run("pylint src")
