bfd5d5696fecc095c067a6e4d852b109482ea745
root <root@DESKTOP-S6N21NG.localdomain>
2022-11-15 13:09:17 +0200

    coverage added to gitignore

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,27 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_lataaminen_kasvattaa_saldoa(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
+        
+    def test_rahan_ottaminen_toimii(self):
+        self.maksukortti.ota_rahaa(1000)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 0.00 euroa")
+
+
+    def test_saldo_vahenee_oikein_jos_rahaa_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+
+    def test_saldo_ei_muutu_jos_rahaa_ei_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(2000)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_metodi_palauttaa_true_jos_rahat_riittivat_muuten_false(self):
+        arvo = self.maksukortti.ota_rahaa(1000)
+        self.assertEqual(arvo, True)
