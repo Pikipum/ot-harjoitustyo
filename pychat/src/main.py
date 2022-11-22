ddc218683fcdef2cf4118a498302c1b9b32f7d34
Markus Tervonen <markus.tervonen@hotmail.com>
2022-11-22 18:17:06 +0200

    function to add users, check password

--- pychat/src/main.py
+++ pychat/src/main.py
@@ -2,9 +2,27 @@ from user import User
 from message import Message
 from ui import UI
 from tkinter import Tk, ttk
+import tkMessageBox
 
 window = Tk()
 window.title("PyChat")
+
+# Users are stored in a dictionary. Their username returns the User object
+users = {}
+
+# Create a new user and add it to the dictionary of users
+def add_user(name, password):
+    users[name: User(name, [], password)]
+
+# Fetch user object from users dictionary, check if password is a match
+def check_log_in(username, password):
+    if password == users[username].get_password:
+        tkMessageBox.showinfo( "Log in", "Log in succesful")
+        return 1
+    tkMessageBox.showinfo( "Log in", "Wrong password")
+    return 0
+
+log_in = Tk.Button(window, text ="Log in", command = check_log_in)
 
 ui = UI(window)
 ui.start()
--- pychat/src/user.py
+++ pychat/src/user.py
@@ -2,6 +2,10 @@ from chat_message import Message
 
 class User:
     # Users have a name and a list of messages they have sent
-    def __init__(self, name, msghistory):
+    def __init__(self, name, msghistory, password):
         self.name = name
         self.msghistory = msghistory
+        self.password = password
+
+    def get_password():
+        return self.password
